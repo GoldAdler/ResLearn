6da78745005d733a95fc81cc0903d81b892cd72c
Julian <Julian@Juli>
2018-04-20 18:20:18 +0200

    Fenster Einstellungen, package view erstellt

--- ResLearn/src/reslearn/view/View.java
+++ ResLearn/src/reslearn/view/View.java
@@ -1,18 +1,20 @@
+package reslearn.view;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class View extends Application{
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	public void start (Stage stage) throws Exception {
-		Parent root = (Parent) FXMLLoader.load(getClass().getResource("/reslearn.view/mainmenu.fxml"));
+		Parent root = (Parent) FXMLLoader.load(getClass().getResource("Einstellungen.fxml"));
 		stage.setScene(new Scene(root));
 		stage.setTitle("ResLearn");
 		stage.show();
 	}
-
 }
