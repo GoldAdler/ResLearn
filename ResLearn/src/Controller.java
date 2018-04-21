da8faa5bca819be8f0f4a3ee88400c6a70be6673
Moritz <Moritz@MoritzPC>
2018-04-21 16:00:21 +0200

    Views + Controller

--- ResLearn/src/Controller.java
+++ ResLearn/src/Controller.java
@@ -2,25 +2,45 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class Controller {
 	@FXML
-	Button uebungauswaehlen = new Button();
+	Button uebungauswaehlen;
 	@FXML
-	Button aufgabeerstellen = new Button();
+	Button aufgabeerstellen;
 	@FXML
-	Button aufgabeladen = new Button();
+	Button aufgabeladen;
 	@FXML
-	Button tutorial = new Button();
+	Button tutorial;
 	@FXML
-	Button einstellungen = new Button();
+	Button einstellungen;
 
 	//Hallo T‰t‰r‰
+	public Controller() {
+		uebungauswaehlen = new Button();
+		aufgabeerstellen = new Button();
+		aufgabeladen = new Button();
+		tutorial = new Button();
+		einstellungen = new Button();
+	}
 
 	@FXML
-    public void weiter() {
+    public void weiter(ActionEvent event) throws Exception {
 		//weiter zur n√§chsten Funktion
+		System.out.println("Hello World");
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/reslearn.view/Vorlage.fxml"));
+		Scene newScene = new Scene(root);
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(newScene);
+		window.show();
 	}
 	public void zurueck() {
 		//zum vorherigen Fenster zur√ºck
---
+++ ResLearn/src/reslearn/view/View.java
@@ -0,0 +1,27 @@
+package reslearn.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class View extends Application{
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	public void start (Stage stage) throws Exception {
+		
+		
+		
+		
+		
+		
+		Parent root = (Parent) FXMLLoader.load(getClass().getResource("mainmenu.fxml"));
+		stage.setScene(new Scene(root));
+		stage.setTitle("ResLearn");
+		stage.show();
+	}
+}
