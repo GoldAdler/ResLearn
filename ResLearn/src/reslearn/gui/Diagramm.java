c6d85589d5de044e052e59bfc202d33cc00508d1
Julian <Julian@Juli>
2018-04-25 08:09:55 +0200

    GUI Test Methode hinzugefügt

--- ResLearn/src/reslearn/gui/Diagramm.java
+++ ResLearn/src/reslearn/gui/Diagramm.java
@@ -2,6 +2,8 @@ package reslearn.gui;
 
 import javafx.scene.canvas.*;
 import javafx.scene.paint.Color;
+import reslearn.model.paket.ResEinheit;
+import reslearn.gui.ResFeld;
 
 public class Diagramm {
 	
@@ -10,44 +12,37 @@ public class Diagramm {
 	private int abstandY = 20;
 	private int spaltX = 5;
 	private int spaltY = 5;
+	private ResEinheit[][] koordinatenSystem;
+	ResFeld[][] resfeld;
+	ResFeld feld;
+	int i;
+	int j;
 	
     public void zeichneCanvas(GraphicsContext gc, Canvas canvas) {
+
         gc.setStroke(Color.GRAY);
         gc.strokeLine(0, canvas.getHeight(), 0, 0);
         gc.strokeLine(0, canvas.getHeight(), canvas.getWidth(), canvas.getHeight());
         gc.strokeLine(0, 0, canvas.getWidth(), 0);
         gc.strokeLine(canvas.getWidth(), canvas.getHeight(), canvas.getWidth(), 0);
-        /*    P2
-         *     |
-         *    P1 
-         *    	P1--P2
-         */
+
         gc.setLineWidth(4);
         gc.strokeLine(abstandX, canvas.getHeight()-abstandY, abstandX, abstandY - spaltY);
         gc.strokeLine(abstandX, canvas.getHeight()-abstandY, canvas.getWidth()-abstandX + spaltX, canvas.getHeight()-abstandY);
-        zeichneKoordinatensystem(gc,canvas);
-
-    }
-
-    public void zeichneKoordinatensystem(GraphicsContext gc, Canvas canvas) {
-//			43*28
-    	for(int i=0; i<430;i+=10) {
-    		for(int j=0; j<280;j+=10) {
-    	    	int random = (int)(Math.random() * 255);
-    	        gc.setFill(Color.rgb(random/1, 200, random/8));
-    	        gc.setLineWidth(1);
-    	        gc.strokeRect(spaltX + abstandX + i*2, canvas.getHeight() - spaltY - abstandY -20 - j*2, 20, 20);
-    	        gc.fillRect(spaltX + abstandX + i*2, canvas.getHeight() - spaltY - abstandY -20 - j*2, 20, 20);
-    		}
-    	}
-    	        
-    }
-    
-    public void zeichePaket() {
-    	
+        
+        koordinatenSystem = new ResEinheit[43][28];
+		resfeld = new ResFeld[i][j];
+		for(i=0; i<430;i+=10) {
+			for(j=0; j<280;j+=10) {
+//				feld[i][j] = new ResFeld();
+				feld.zeichneDiagramm(gc, canvas, abstandX, abstandY, spaltX, spaltY, i, j);
+				
+			}
+		}
+//		feld[120][40].setzeFarbe(gc);
     }
 	
     public void zeichneTeilpaket() {
-    	
+
     }
 }
