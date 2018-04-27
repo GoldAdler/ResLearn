c815942cb006998c2c0e10b98ec3b046dcf21e33
Klemens <Klemens@Klemens.fritz.box>
2018-04-27 18:08:26 +0200

    Algo Kapazitätstreu Implementierung

--- ResLearn/src/reslearn/model/algorithmus/AlgoKapazitaetstreu.java
+++ ResLearn/src/reslearn/model/algorithmus/AlgoKapazitaetstreu.java
@@ -1,11 +1,18 @@
 package reslearn.model.algorithmus;
 
+import java.util.LinkedList;
+
 import reslearn.model.paket.ResEinheit;
+import reslearn.model.paket.Teilpaket;
+import reslearn.model.paket.Vektor2i;
 import reslearn.model.resCanvas.ResCanvas;
 
 public class AlgoKapazitaetstreu extends Algorithmus {
 
 	private static AlgoKapazitaetstreu algoKapazitaetstreu;
+
+	// TODO: Vorläufige Integer. Wieder löschen!!!!
+	private static int maxBegrenzung = 5;
 
 	private AlgoKapazitaetstreu() {
 	}
@@ -19,8 +26,76 @@ public class AlgoKapazitaetstreu extends Algorithmus {
 
 	@Override
 	public ResEinheit[][] algoDurchfuehren(ResCanvas resCanvas) {
+		ResEinheit[][] koordinatenSystem = resCanvas.getKoordinatenSystem();
+		LinkedList<Teilpaket> teilpaketListe = ueberpruefeObergrenze(resCanvas, koordinatenSystem);
+		LinkedList<Teilpaket> unterhalbUndRechts = new LinkedList<Teilpaket>();
+		Vektor2i position = null;
+		ResEinheit unterhalb;
 
+		for (Teilpaket teilpaket : teilpaketListe) {
+			LOOP: for (ResEinheit res : teilpaket.getResEinheitListe()) {
+
+				position = res.getPosition();
+				int x = position.getxKoordinate();
+				for (int y = position.getyKoordinate(); y > ResCanvas.koorHoehe; y++) {
+					unterhalb = koordinatenSystem[y][x];
+
+					// wenn unterhalb der aktuellen resEinheit nichts liegt,
+					// wir die Schleife abgebrochen und die nächste resEinheit betrachtet
+					if (unterhalb == null) {
+						break;
+					}
+					// wenn unterhalb der aktuellen resEinheit eine Reseinheit liegt, die
+					// vom selben Teilpaket ist, wird die Betrachtung des aktuellen Teilpaketes
+					// abgebrochen
+					// die variable position wird auf die vorhergegane ResEinheit gesetzt
+					if (teilpaket == unterhalb.getTeilpaket()) {
+						int index = teilpaket.getResEinheitListe().indexOf(res);
+						position = teilpaket.getResEinheitListe().get(index - 1).getPosition();
+						break LOOP;
+					}
+
+					unterhalbUndRechts.add(unterhalb.getTeilpaket());
+					resCanvas.entfernenTeilpaket(unterhalb.getTeilpaket());
+				}
+			}
+
+			for (int y = position.getyKoordinate() - 1; y > ResCanvas.koorHoehe; y++) {
+				for (int x = position.getxKoordinate() + 1; x < ResCanvas.koorBreite; x++) {
+					unterhalb = koordinatenSystem[y][x];
+					if (unterhalb != null) {
+						unterhalbUndRechts.add(unterhalb.getTeilpaket());
+						resCanvas.entfernenTeilpaket(unterhalb.getTeilpaket());
+					}
+				}
+			}
+
+		}
 		return null;
+	}
+
+	/**
+	 * Überprüft ob Teilpakete über der Maximalgrenze an parallel arbeitenden
+	 * Mitarbeitern liegen. Wenn ja, wer diesen in eine LinkList gespeichtert.
+	 *
+	 * @param resCanvas
+	 * @param koordinatenSystem
+	 * @return teilpaketListe
+	 */
+	private LinkedList<Teilpaket> ueberpruefeObergrenze(ResCanvas resCanvas, ResEinheit[][] koordinatenSystem) {
+		LinkedList<Teilpaket> teilpaketListe = new LinkedList<Teilpaket>();
+		ResEinheit tempResEinheit;
+		for (int x = 0; x < ResCanvas.koorBreite; x++) {
+
+			// maxBegrenzung nicht -1, weil Paket innerhalb der Begrenzung noch valide ist!
+			tempResEinheit = koordinatenSystem[maxBegrenzung][x];
+			if (tempResEinheit != null) {
+				teilpaketListe.add(koordinatenSystem[maxBegrenzung][x].getTeilpaket());
+				resCanvas.entfernenTeilpaket(tempResEinheit.getTeilpaket());
+			}
+		}
+
+		return teilpaketListe;
 	}
 
 }
--- ResLearn/src/reslearn/model/resCanvas/ResCanvas.java
+++ ResLearn/src/reslearn/model/resCanvas/ResCanvas.java
@@ -1,10 +1,12 @@
 package reslearn.model.resCanvas;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 import reslearn.model.paket.Arbeitspaket;
 import reslearn.model.paket.ArbeitspaketZustand;
 import reslearn.model.paket.ResEinheit;
+import reslearn.model.paket.Teilpaket;
 import reslearn.model.paket.Vektor2i;
 
 public class ResCanvas {
@@ -14,6 +16,7 @@ public class ResCanvas {
 	private ResEinheit[][] koordinatenSystem;
 	public static final int koorHoehe = 28;
 	public static final int koorBreite = 43;
+	private int maxMitarbeiter = koorHoehe - 1;
 
 	public ResCanvas() {
 		arbeitspaketZustandListe = new LinkedList<ArbeitspaketZustand>();
@@ -22,10 +25,94 @@ public class ResCanvas {
 		koordinatenSystem = new ResEinheit[koorHoehe][koorBreite];
 	}
 
+	public ResCanvas(int maxMitarbeiter) {
+		arbeitspaketZustandListe = new LinkedList<ArbeitspaketZustand>();
+		aktuellerZustand = new ArbeitspaketZustand();
+		// arbeitspaketZustandListe.add(aktuellerZustand);
+		koordinatenSystem = new ResEinheit[koorHoehe][koorBreite];
+		this.maxMitarbeiter = maxMitarbeiter;
+	}
+
 	public void updatePosition(ResEinheit resEinheit, Vektor2i altePosition) {
 		this.koordinatenSystem[altePosition.getyKoordinate()][altePosition.getxKoordinate()] = null;
 		Vektor2i position = resEinheit.getPosition();
 		this.koordinatenSystem[position.getyKoordinate()][position.getxKoordinate()] = resEinheit;
+	}
+
+	/**
+	 * entfernen des Teilpakets aus dem Koordinatensystem! Teilpaket ist nicht
+	 * gelöscht, sondern weiß immer noch die Position
+	 *
+	 * @param teilpaket
+	 */
+	public void entfernenTeilpaket(Teilpaket teilpaket) {
+		Vektor2i position;
+		for (ResEinheit resEinheit : teilpaket.getResEinheitListe()) {
+			position = resEinheit.getPosition();
+			koordinatenSystem[position.getyKoordinate()][position.getxKoordinate()] = null;
+		}
+	}
+
+	public void herunterfallen(Teilpaket teilpaket) {
+		Vektor2i position;
+		ResEinheit unterhalb;
+		ResEinheit oberhalb;
+		ArrayList<ResEinheit> neuesTeilpaket = new ArrayList<ResEinheit>();
+		for (ResEinheit res : teilpaket.getResEinheitListe()) {
+
+			position = res.getPosition();
+			int x = position.getxKoordinate();
+			int y = position.getyKoordinate();
+
+			if (y != ResCanvas.koorHoehe - 1) {
+				unterhalb = koordinatenSystem[y + 1][x];
+
+				if (unterhalb == null) {
+
+					for (int yPos = y; yPos >= 0; yPos--) {
+						oberhalb = koordinatenSystem[yPos][x];
+						if (oberhalb == null) {
+							break;
+						}
+
+						neuesTeilpaket.add(oberhalb);
+					}
+				} else {
+					break;
+				}
+
+				// TODO: da mache mer weiter ihr lappos
+				// - sortieren der ArrayList
+				// - ResEinheiten aus dem altten Teilpaket entfernen
+				// - neues Teilpaket anlegen und ARbeitspaket zuordnen
+				// - neuem Teilpaket Arraylist übergeben
+				// - nach unten fahren lassen
+				// - Rekursives Problem bedenken (wenn kleines Paketchen unter packtneahn unt
+				// dkaghhe t)
+
+			}
+
+		}
+
+		// TODO: aus anderem Algo rauskopiert. löschen wenn nicht mehr gebraucht
+		// // wenn unterhalb der aktuellen resEinheit nichts liegt,
+		// // wir die Schleife abgebrochen und die nächste resEinheit betrachtet
+		// if (unterhalb == null) {
+		// break;
+		// }
+		// // wenn unterhalb der aktuellen resEinheit eine Reseinheit liegt, die
+		// // vom selben Teilpaket ist, wird die Betrachtung des aktuellen Teilpaketes
+		// // abgebrochen
+		// // die variable position wird auf die vorhergegane ResEinheit gesetzt
+		// if (teilpaket == unterhalb.getTeilpaket()) {
+		// int index = teilpaket.getResEinheitListe().indexOf(res);
+		// position = teilpaket.getResEinheitListe().get(index - 1).getPosition();
+		// break LOOP;
+		// }
+		//
+		// unterhalbUndRechts.add(unterhalb.getTeilpaket());
+		// resCanvas.entfernenTeilpaket(unterhalb.getTeilpaket());
+
 	}
 
 	/**
@@ -113,6 +200,14 @@ public class ResCanvas {
 
 	public void setKoordinatenSystem(ResEinheit[][] koordinatenSystem) {
 		this.koordinatenSystem = koordinatenSystem;
+	}
+
+	public int getMaxMitarbeiter() {
+		return maxMitarbeiter;
+	}
+
+	public void setMaxMitarbeiter(int maxMitarbeiter) {
+		this.maxMitarbeiter = maxMitarbeiter;
 	}
 
 }
