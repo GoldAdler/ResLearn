c8a2e45091a2d4186d25f12fb6b06fda47f28ee1
Lukasjuhu <Lukasjuhu>
2018-04-29 13:06:48 +0200

    Controller ModusAuswaehlen erweitert

--- ResLearn/src/reslearn/gui/controller/ControllerModusAuswaehlen.java
+++ ResLearn/src/reslearn/gui/controller/ControllerModusAuswaehlen.java
@@ -1,47 +1,23 @@
 package reslearn.gui.controller;
 
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class ControllerModusAuswaehlen {
 	@FXML
-	private ImageView ersterSchritt;
+	private Group ersterSchritt;
 	@FXML
-	private ImageView uebungsmodus;
+	private Group uebungsmodus;
 	@FXML
-	private ImageView loesungsmodus;
+	private Group loesungsmodus;
 
 
-	@FXML
-	public void initialize(){
-		ersterSchritt.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-		    @Override
-		    public void handle(MouseEvent event) {
-		    	Scene newScene;
-		    	System.out.println("Erfolg");
-				try {		
-					newScene = new Scene(FXMLLoader.load(getClass().getResource("./../fxml/Uebungsmodus")));
-					System.out.println("Hurra");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-		    	
-		    }
-		});
-	}
-	
 	@FXML
     public void weiter(ActionEvent event) throws Exception{
 		Scene newScene = null;
