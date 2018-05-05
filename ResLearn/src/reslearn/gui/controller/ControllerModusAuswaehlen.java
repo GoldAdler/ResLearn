18a9fe03e396ea03f3506bc1dc1ef8d948f2b4dc
Lukasjuhu <Lukasjuhu>
2018-05-05 11:58:07 +0200

    ModusAuswaehlen.fxml und ModusAuswaehlenController fertiggestellt

--- ResLearn/src/reslearn/gui/controller/ControllerModusAuswaehlen.java
+++ ResLearn/src/reslearn/gui/controller/ControllerModusAuswaehlen.java
@@ -6,44 +6,52 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import reslearn.gui.View;
 
 public class ControllerModusAuswaehlen {
 	
 	@FXML
-	private Node ersterSchritt;
+	private Button ersterSchritt;
 	@FXML
-	private Node uebungsmodus;
+	private Button uebungsmodus;
 	@FXML
-	private Group loesungsmodus;
+	private Button loesungsmodus;
 
 	@FXML
     public void weiter(ActionEvent event) throws Exception{
 		Scene newScene;
 
 		if(event.getSource()==ersterSchritt) {
-			newScene = new Scene(FXMLLoader.load(getClass().getResource("../fxml/Uebungsmodus.fxml")));
-			Stage stage = new Stage();
-			stage.setTitle("ResLearn");
-			stage.setMaximized(true);
-			stage.setScene(newScene);
-			stage.show();
+//			newScene = new Scene(FXMLLoader.load(getClass().getResource("../fxml/Uebungsmodus.fxml")));
+//			Stage stage = new Stage();
+//			stage.setTitle("ResLearn");
+//			stage.setMaximized(true);
+//			stage.setScene(newScene);
+//			stage.show();
+			View view = new View();
+			view.start(View.classStage);
 			((Node) (event.getSource())).getScene().getWindow().hide();
 		}else if(event.getSource()==uebungsmodus) {
-			newScene = new Scene(FXMLLoader.load(getClass().getResource("../fxml/Uebungsmodus.fxml")));
-			Stage stage = new Stage();
-			stage.setTitle("ResLearn");
-			stage.setMaximized(true);
-			stage.setScene(newScene);
-			stage.show();
+//			newScene = new Scene(FXMLLoader.load(getClass().getResource("../fxml/Uebungsmodus.fxml")));
+//			Stage stage = new Stage();
+//			stage.setTitle("ResLearn");
+//			stage.setMaximized(true);
+//			stage.setScene(newScene);
+//			stage.show();
+			View view = new View();
+			view.start(View.classStage);
 			((Node) (event.getSource())).getScene().getWindow().hide();
 		}else if(event.getSource()==loesungsmodus) {
-			newScene = new Scene(FXMLLoader.load(getClass().getResource("../fxml/Uebungsmodus.fxml")));
-			Stage stage = new Stage();
-			stage.setTitle("ResLearn");
-			stage.setMaximized(true);
-			stage.setScene(newScene);
-			stage.show();
+//			newScene = new Scene(FXMLLoader.load(getClass().getResource("../fxml/Uebungsmodus.fxml")));
+//			Stage stage = new Stage();
+//			stage.setTitle("ResLearn");
+//			stage.setMaximized(true);
+//			stage.setScene(newScene);
+//			stage.show();
+			View view = new View();
+			view.start(View.classStage);
 			((Node) (event.getSource())).getScene().getWindow().hide();
 		} 
 	}
