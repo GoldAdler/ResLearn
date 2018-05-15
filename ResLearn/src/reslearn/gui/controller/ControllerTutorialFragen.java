0c18db8036f99bb5a96d91bae88942e57af030dc
Lukasjuhu <Lukasjuhu>
2018-05-15 16:25:01 +0200

    Tutorial und Zurueck+Homebutton fertig

--- ResLearn/src/reslearn/gui/controller/ControllerTutorialFragen.java
+++ ResLearn/src/reslearn/gui/controller/ControllerTutorialFragen.java
@@ -3,7 +3,6 @@ package reslearn.gui.controller;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -13,7 +12,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import reslearn.gui.fxml.TutorialVideo;
@@ -22,6 +20,7 @@ public class ControllerTutorialFragen extends Controller {
 
 	int counter = 0;
 	int counterFrageAntwort = 1;
+	int counterRichtigeAntworten = 0;
 
 	@FXML
 	private Button zurueck;
@@ -31,6 +30,8 @@ public class ControllerTutorialFragen extends Controller {
 	private Button weiter;
 	@FXML
 	private Label label;
+	@FXML
+	private Label labelErgebnis;
 	@FXML
 	private ToggleGroup tg;
 	@FXML
@@ -82,17 +83,30 @@ public class ControllerTutorialFragen extends Controller {
 	public void weiter(ActionEvent event) {
 		System.out.println("Button gedrückt" + counter);
 		if (counter < 4) {
-			if (counterFrageAntwort == 1) {
-				frage(counter);
-				counterFrageAntwort = 2;
-			} else {
+			if (counter == 0) {
 				antwort(counter);
-				counterFrageAntwort = 1;
 				counter += 1;
+			} else {
+				if (counterFrageAntwort == 1) {
+					frage(counter);
+					counterFrageAntwort = 2;
+				} else {
+					antwort(counter);
+					counterFrageAntwort = 1;
+					counter++;
+				}
 			}
-
 		} else if (counter == 4) {
-			counter = 0;
+			counter++;
+			if (counterRichtigeAntworten > 2) {
+				labelErgebnis.setText(
+						"Es wurden " + counterRichtigeAntworten + " von 4 Fragen richtig beantwortet.\nKlasse!!");
+			} else {
+				labelErgebnis.setText(
+						"Es wurden " + counterRichtigeAntworten + " von 4 Fragen richtig beantwortet.\nSchäm dich!!");
+			}
+			labelErgebnis.setVisible(true);
+		} else if (counter == 5) {
 			Scene newScene;
 			Parent root;
 			alleFenster.add("../fxml/TutorialFragen.fxml");
@@ -113,9 +127,10 @@ public class ControllerTutorialFragen extends Controller {
 	}
 
 	public void antwort(int counter) {
-		if (counter == 0) {
+		switch (counter) {
+		case 0:
 			if (rb1.isSelected()) {
-				label.setText("Antwort 1 ist richtig");
+				label.setText("Antwort " + rb1.getText() + " ist richtig");
 				rb1.setTextFill(Color.GREEN);
 				rb2.setDisable(true);
 				rb2.setTextFill(Color.RED);
@@ -123,9 +138,17 @@ public class ControllerTutorialFragen extends Controller {
 				rb3.setTextFill(Color.RED);
 				rb4.setDisable(true);
 				rb4.setTextFill(Color.RED);
+				counterRichtigeAntworten++;
 			} else {
-				// label.setText("Antwort " + tg.getSelectedToggle().getUserData() + " war
-				// falsch");
+				if (rb2.isSelected()) {
+					label.setText("Antwort " + rb2.getText() + " war falsch");
+				} else if (rb3.isSelected()) {
+					label.setText("Antwort " + rb3.getText() + " war falsch");
+				} else if (rb4.isSelected()) {
+					label.setText("Antwort " + rb4.getText() + " war falsch");
+				} else {
+					label.setText("Keine Antwort ist falsch, netter Versuch");
+				}
 				rb1.setTextFill(Color.GREEN);
 				rb2.setDisable(true);
 				rb2.setTextFill(Color.RED);
@@ -134,48 +157,105 @@ public class ControllerTutorialFragen extends Controller {
 				rb4.setDisable(true);
 				rb4.setTextFill(Color.RED);
 			}
-
-		} else if (counter == 1) {
-			label.setText("Antwort 1");
-			tg = new ToggleGroup();
-			rb1.setText("Richtig 1");
-			rb1.setToggleGroup(tg);
-			rb2.setText("Falsch 2");
-			rb2.setToggleGroup(tg);
-			rb3.setText("Falsch 3");
-			rb3.setToggleGroup(tg);
-			rb4.setText("Falsch 4");
-			rb4.setToggleGroup(tg);
-		} else if (counter == 2) {
-			label.setText("Antwort 2");
-			tg = new ToggleGroup();
-			rb1.setText("Richtig 11");
-			rb1.setToggleGroup(tg);
-			rb2.setText("Falsch 22");
-			rb2.setToggleGroup(tg);
-			rb3.setText("Falsch 33");
-			rb3.setToggleGroup(tg);
-			rb4.setText("Falsch 44");
-			rb4.setToggleGroup(tg);
-		} else if (counter == 3) {
-			label.setText("Antwort 3");
-			tg = new ToggleGroup();
-			rb1.setText("Falsch 111");
-			rb1.setToggleGroup(tg);
-			rb2.setText("Richtig 222");
-			rb2.setToggleGroup(tg);
-			rb3.setText("Falsch 333");
-			rb3.setToggleGroup(tg);
-			rb4.setText("Falsch 444");
-			rb4.setToggleGroup(tg);
+			break;
+		case 1:
+			if (rb1.isSelected()) {
+				label.setText("Antwort " + rb1.getText() + " ist richtig");
+				rb1.setTextFill(Color.GREEN);
+				rb2.setDisable(true);
+				rb2.setTextFill(Color.RED);
+				rb3.setDisable(true);
+				rb3.setTextFill(Color.RED);
+				rb4.setDisable(true);
+				rb4.setTextFill(Color.RED);
+				counterRichtigeAntworten++;
+			} else {
+				if (rb2.isSelected()) {
+					label.setText("Antwort " + rb2.getText() + " war falsch");
+				} else if (rb3.isSelected()) {
+					label.setText("Antwort " + rb3.getText() + " war falsch");
+				} else if (rb4.isSelected()) {
+					label.setText("Antwort " + rb4.getText() + " war falsch");
+				} else {
+					label.setText("Keine Antwort ist falsch, netter Versuch");
+				}
+				rb1.setTextFill(Color.GREEN);
+				rb2.setDisable(true);
+				rb2.setTextFill(Color.RED);
+				rb3.setDisable(true);
+				rb3.setTextFill(Color.RED);
+				rb4.setDisable(true);
+				rb4.setTextFill(Color.RED);
+			}
+			break;
+		case 2:
+			if (rb1.isSelected()) {
+				label.setText("Antwort " + rb1.getText() + " ist richtig");
+				rb1.setTextFill(Color.GREEN);
+				rb2.setDisable(true);
+				rb2.setTextFill(Color.RED);
+				rb3.setDisable(true);
+				rb3.setTextFill(Color.RED);
+				rb4.setDisable(true);
+				rb4.setTextFill(Color.RED);
+				counterRichtigeAntworten++;
+			} else {
+				if (rb2.isSelected()) {
+					label.setText("Antwort " + rb2.getText() + " war falsch");
+				} else if (rb3.isSelected()) {
+					label.setText("Antwort " + rb3.getText() + " war falsch");
+				} else if (rb4.isSelected()) {
+					label.setText("Antwort " + rb4.getText() + " war falsch");
+				} else {
+					label.setText("Keine Antwort ist falsch, netter Versuch");
+				}
+				rb1.setTextFill(Color.GREEN);
+				rb2.setDisable(true);
+				rb2.setTextFill(Color.RED);
+				rb3.setDisable(true);
+				rb3.setTextFill(Color.RED);
+				rb4.setDisable(true);
+				rb4.setTextFill(Color.RED);
+			}
+			break;
+		case 3:
+			if (rb1.isSelected()) {
+				label.setText("Antwort " + rb1.getText() + " ist richtig");
+				rb1.setTextFill(Color.GREEN);
+				rb2.setDisable(true);
+				rb2.setTextFill(Color.RED);
+				rb3.setDisable(true);
+				rb3.setTextFill(Color.RED);
+				rb4.setDisable(true);
+				rb4.setTextFill(Color.RED);
+				counterRichtigeAntworten++;
+			} else {
+				if (rb2.isSelected()) {
+					label.setText("Antwort " + rb2.getText() + " war falsch");
+				} else if (rb3.isSelected()) {
+					label.setText("Antwort " + rb3.getText() + " war falsch");
+				} else if (rb4.isSelected()) {
+					label.setText("Antwort " + rb4.getText() + " war falsch");
+				} else {
+					label.setText("Keine Antwort ist falsch, netter Versuch");
+				}
+				rb1.setTextFill(Color.GREEN);
+				rb2.setDisable(true);
+				rb2.setTextFill(Color.RED);
+				rb3.setDisable(true);
+				rb3.setTextFill(Color.RED);
+				rb4.setDisable(true);
+				rb4.setTextFill(Color.RED);
+			}
+			break;
 		}
 	}
 
 	public void frage(int counter) {
-		if (counter == 0) {
+		switch (counter) {
+		case 0:
 			selektieren();
 			label.setText("Frage 0");
-			;
 			tg = new ToggleGroup();
 
 			rb1.setText("Antwort 01");
@@ -193,7 +273,9 @@ public class ControllerTutorialFragen extends Controller {
 			rb4.setText("Antwort 04");
 			rb4.setUserData(rb4.getText());
 			rb4.setToggleGroup(tg);
-		} else if (counter == 1) {
+			break;
+
+		case 1:
 			selektieren();
 			label.setText("Frage 1");
 			tg = new ToggleGroup();
@@ -213,28 +295,51 @@ public class ControllerTutorialFragen extends Controller {
 			rb4.setText("Antwort 4");
 			rb4.setUserData(rb4.getText());
 			rb4.setToggleGroup(tg);
-		} else if (counter == 2) {
+			break;
+
+		case 2:
+			selektieren();
 			label.setText("Frage 2");
 			tg = new ToggleGroup();
+
 			rb1.setText("Antwort 11");
+			rb1.setUserData(rb1.getText());
 			rb1.setToggleGroup(tg);
+
 			rb2.setText("Antwort 22");
+			rb2.setUserData(rb2.getText());
 			rb2.setToggleGroup(tg);
+
 			rb3.setText("Antwort 33");
+			rb3.setUserData(rb3.getText());
 			rb3.setToggleGroup(tg);
+
 			rb4.setText("Antwort 44");
+			rb4.setUserData(rb4.getText());
 			rb4.setToggleGroup(tg);
-		} else if (counter == 3) {
+			break;
+
+		case 3:
+			selektieren();
 			label.setText("Frage 3");
 			tg = new ToggleGroup();
+
 			rb1.setText("Antwort 111");
+			rb1.setUserData(rb1.getText());
 			rb1.setToggleGroup(tg);
+
 			rb2.setText("Antwort 222");
+			rb2.setUserData(rb2.getText());
 			rb2.setToggleGroup(tg);
+
 			rb3.setText("Antwort 333");
+			rb3.setUserData(rb3.getText());
 			rb3.setToggleGroup(tg);
+
 			rb4.setText("Antwort 444");
+			rb4.setUserData(rb4.getText());
 			rb4.setToggleGroup(tg);
+			break;
 		}
 
 	}
@@ -250,29 +355,4 @@ public class ControllerTutorialFragen extends Controller {
 		rb4.setTextFill(Color.BLACK);
 	}
 
-	@FXML
-	public void home() throws Exception {
-		home.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-			Scene newScene;
-
-			@Override
-			public void handle(MouseEvent event) {
-				alleFenster.add("../fxml/UebungAuswaehlen.fxml");
-				Parent root;
-				try {
-					root = FXMLLoader.load(getClass().getResource(hauptmenue()));
-					newScene = new Scene(root);
-					Stage stage = new Stage();
-					stage.setTitle("ResLearn");
-					stage.setMaximized(true);
-					stage.setScene(newScene);
-					stage.show();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				((Node) (event.getSource())).getScene().getWindow().hide();
-			}
-		});
-	}
 }
