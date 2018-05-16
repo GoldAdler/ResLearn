07ee3d7a68a8f9c39f57c05e136dbe52444a7ac9
Lukasjuhu <Lukasjuhu>
2018-05-16 15:47:33 +0200

    Aufgabe erstellen fertig und Aufgabe laden fast fertig

--- ResLearn/src/reslearn/gui/controller/ControllerAufgabeLaden.java
+++ ResLearn/src/reslearn/gui/controller/ControllerAufgabeLaden.java
@@ -1,6 +1,9 @@
 package reslearn.gui.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,7 +12,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import reslearn.gui.ImportExport.CsvReader;
+import reslearn.model.paket.Arbeitspaket;
 
 public class ControllerAufgabeLaden extends Controller {
 
@@ -17,6 +24,100 @@ public class ControllerAufgabeLaden extends Controller {
 	private Button zurueck;
 	@FXML
 	private Button home;
+	@FXML
+	private Label labellDateiname;
+	@FXML
+	private Button laden;
+	@FXML
+	private Button dateiauswaehlen;
+
+	Arbeitspaket[] paketeArray;
+	private String name;
+	private String dateipfad = "C:\\Users\\Eric Botor\\git\\ResLearn\\";
+
+	@FXML
+	public void dateiauswaehlen() {
+		FileChooser fileChooser = new FileChooser();
+		File defaultDirectory = new File(dateipfad);
+		fileChooser.setInitialDirectory(defaultDirectory);
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+		fileChooser.getExtensionFilters().add(extFilter);
+		File file = fileChooser.showOpenDialog(dateiauswaehlen.getScene().getWindow());
+		name = file.getName();
+		labellDateiname.setText("Wollen Sie die " + name + " Aufgabe laden?");
+		labellDateiname.setVisible(true);
+		laden.setDisable(false);
+	}
+
+	@FXML
+	public void laden(ActionEvent event) {
+		try {
+
+			CsvReader arbeitspaketImport = new CsvReader(dateipfad + name);
+
+			arbeitspaketImport.readHeaders();
+			ArrayList<Arbeitspaket> pakete = new ArrayList<Arbeitspaket>();
+
+			while (arbeitspaketImport.readRecord()) {
+
+				Arbeitspaket ap = new Arbeitspaket();
+
+				try {
+					String zeile = arbeitspaketImport.get(0);
+					String[] spalten = zeile.split(";");
+
+					if (spalten.length == 8) {
+						ap.setId(spalten[0]);
+						ap.setFaz(Integer.valueOf(spalten[1]));
+						ap.setFez(Integer.valueOf(spalten[2]));
+						ap.setSaz(Integer.valueOf(spalten[3]));
+						ap.setSez(Integer.valueOf(spalten[4]));
+						ap.setVorgangsdauer(Integer.valueOf(spalten[5]));
+						ap.setMitarbeiteranzahl(Integer.valueOf(spalten[6]));
+						ap.setAufwand(Integer.valueOf(spalten[7]));
+					}
+
+					pakete.add(ap);
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+				}
+			}
+
+			arbeitspaketImport.close();
+
+			paketeArray = getArbeitspaketArray(pakete);
+
+			System.out.println("Gib was aus " + pakete.get(3));
+
+			for (Arbeitspaket ap : paketeArray) {
+				System.out.print(ap.getId().toString());
+				System.out.print(", ");
+				System.out.print(String.valueOf(ap.getFaz()));
+				System.out.print(", ");
+				System.out.print(String.valueOf(ap.getFez()));
+				System.out.print(", ");
+				System.out.print(String.valueOf(ap.getSaz()));
+				System.out.print(", ");
+				System.out.print(String.valueOf(ap.getSez()));
+				System.out.print(", ");
+				System.out.print(String.valueOf(ap.getVorgangsdauer()));
+				System.out.print(", ");
+				System.out.print(String.valueOf(ap.getMitarbeiteranzahl()));
+				System.out.print(", ");
+				System.out.print(String.valueOf(ap.getAufwand()));
+				System.out.print(", ");
+				System.out.println();
+
+			}
+
+			weiter(event);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@FXML
 	public void home(ActionEvent event) throws Exception {
@@ -54,5 +155,32 @@ public class ControllerAufgabeLaden extends Controller {
 			e.printStackTrace();
 		}
 		((Node) (event.getSource())).getScene().getWindow().hide();
+	}
+
+	public void weiter(ActionEvent event) {
+		Scene newScene;
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("../fxml/ModusAuswaehlen.fxml"));
+			newScene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setTitle("ResLearn");
+			stage.setMaximized(true);
+			stage.setScene(newScene);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		((Node) (event.getSource())).getScene().getWindow().hide();
+	}
+
+	public static Arbeitspaket[] getArbeitspaketArray(ArrayList<Arbeitspaket> pakete) {
+		Arbeitspaket arbeitspakete[] = new Arbeitspaket[pakete.size()];
+		int i = 0;
+		for (Arbeitspaket ap : pakete) {
+			arbeitspakete[i++] = ap;
+		}
+		return arbeitspakete;
 	}
 }
