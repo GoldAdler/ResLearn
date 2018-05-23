a54df5477abb1bfa3c14cc2118a47258413fa21a
Lukas Willburger <willburger@hotmail.de>
2018-05-23 19:44:24 +0200

    Unnützen Code entfernt

--- ResLearn/src/reslearn/gui/ResFeld.java
+++ ResLearn/src/reslearn/gui/ResFeld.java
@@ -14,47 +14,9 @@ public class ResFeld extends Rectangle {
 	private ResEinheit resEinheit;
 
 	private HashMap<Arbeitspaket, Color> arbeitspaketFarben = new HashMap<Arbeitspaket, Color>();
-	private final Random random = new Random();
-
 	public ResFeld(double x, double y, ResEinheit resEinheit) {
 		super(x, y, DisplayCanvas.resFeldBreite, DisplayCanvas.resFeldLaenge);
 		this.resEinheit = resEinheit;
-		//setFill(setFarbe(resEinheit));
-	}
-
-	private Color setFarbe(ResEinheit resEinheit) {
-		Arbeitspaket arbeitspaket = resEinheit.getTeilpaket().getArbeitspaket();
-
-
-		// Arbeitspaket hat bereits eine zugewiesene Farbe
-		if (arbeitspaketFarben.containsKey(arbeitspaket)) {
-			return arbeitspaketFarben.get(arbeitspaket);
-		}
-
-		double hue = random.nextInt(18) * 20.0;
-		double saturation = 0.7d + 0.3d * Math.round(Math.random());// 1.0 for brilliant, 0.0 for dull
-		hue = pruefeFarbeBereitsBenutzt(hue);
-		double luminance = 1.0d; // 1.0 for brighter, 0.0 for black
-		Color color = Color.hsb(hue, saturation, luminance, 0.7);
-		arbeitspaketFarben.put(arbeitspaket, color);
-
-		return color;
-	}
-
-	private double pruefeFarbeBereitsBenutzt(double hue) {
-		for (Map.Entry<Arbeitspaket, Color> entry : arbeitspaketFarben.entrySet()) {
-			double entryHue = entry.getValue().getHue();
-			if (entryHue % 1 > 0.0001) {
-				entryHue = Math.floor(entryHue) + 1;
-			} else {
-				entryHue = Math.floor(entryHue);
-			}
-			if (entryHue == hue) {
-				hue = random.nextInt(18) * 20.0;
-				hue = pruefeFarbeBereitsBenutzt(hue);
-			}
-		}
-		return hue;
 	}
 
 	public ResEinheit getResEinheit() {
