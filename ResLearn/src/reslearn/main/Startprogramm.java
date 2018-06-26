a3a26ea5c18dd18e4cca7ddf5da955a34b23baf6
Lukas Willburger <willburger@hotmail.de>
2018-06-26 09:40:11 +0200

    Update

--- ResLearn/src/reslearn/main/Startprogramm.java
+++ ResLearn/src/reslearn/main/Startprogramm.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Startprogramm extends Application {
@@ -16,23 +14,15 @@ public class Startprogramm extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		try {
-			Parent root = (Parent) FXMLLoader.load(getClass().getResource("/reslearn/gui/fxml/Hauptmenue.fxml"));
 
-			Scene scene = new Scene(root);
+		Parent root = (Parent) FXMLLoader.load(getClass().getResource("/reslearn/gui/fxml/Hauptmenue.fxml"));
 
-			stage.setMaximized(true);
-			stage.setScene(scene);
-			stage.setTitle("ResLearn");
-			stage.show();
-		} catch (Exception e) {
-			String meldung = e.getMessage();
-			Alert alert = new Alert(AlertType.INFORMATION);
-			alert.setTitle("Fehlermeldung");
-			alert.setHeaderText("Fehlermeldung");
-			alert.setContentText(meldung);
-			alert.showAndWait();
-		}
+		Scene scene = new Scene(root);
+
+		stage.setMaximized(true);
+		stage.setScene(scene);
+		stage.setTitle("ResLearn");
+		stage.show();
 
 	}
 }
